1c586dec03b0bfeffe1bb6060c3cdb2229603705
RomuloMenezes <romulo.menezes@gmail.com>
2016-07-13 19:33:09 -0300

    Functional version

---
+++ PrepGenHistData/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PrepGenHistData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "D:\\_GIT\\Projetos\\Novo Site\\planilhas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 42);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(294, 93);
+            this.textBox2.TabIndex = 3;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "D:\\_GIT\\Projetos\\Novo Site\\planilhas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 147);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "PrepGenHistData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ PrepGenHistData/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrepGenHistData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                MessageBox.Show("Please select a source folder", "Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            else
+            {
+                DirectoryInfo rootFolder = new DirectoryInfo(textBox1.Text);
+                Microsoft.Office.Interop.Excel.Application xlSourceApp = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Application xlTargetApp = new Microsoft.Office.Interop.Excel.Application();
+                Workbook xlSourceWorkBook;
+                Workbook xlTargetWorkBook;
+                Worksheet xlCurrSourceWorkSheet;
+                Worksheet xlTargetWorkSheet;
+                int iIndex = 0;
+                int xIndex, yIndex;
+                int year;
+                int targetWBRowIndex = 2;
+                int yUpperLimit;
+                int yOffset;
+                string currMessage = "";
+
+                xlTargetWorkBook = xlTargetApp.Workbooks.Open("D:\\_GIT\\Projetos\\Novo Site\\planilhas\\geração\\SínteseGeração.xlsx");
+                xlTargetWorkSheet = xlTargetWorkBook.Worksheets[1];
+                xlTargetWorkSheet.Cells[1, 1] = "Data";
+                xlTargetWorkSheet.Cells[1, 2] = "Tipo de geração";
+                xlTargetWorkSheet.Cells[1, 3] = "Região";
+                xlTargetWorkSheet.Cells[1, 4] = "Unidade";
+                xlTargetWorkSheet.Cells[1, 5] = "Montante";
+
+                foreach(DirectoryInfo currFolder in rootFolder.GetDirectories("geracao_*")){
+                    if (currFolder.Name.IndexOf("nuclear") > 0){
+                        yUpperLimit = 2;
+                        yOffset = 5;
+                    }
+                    else{
+                        yUpperLimit = 7;
+                        yOffset = 10;
+                    }
+                    xlSourceWorkBook = xlSourceApp.Workbooks.Open(currFolder.GetFiles()[0].FullName);
+                    for (iIndex = 1; iIndex <= xlSourceWorkBook.Worksheets.Count; iIndex++) {
+                        xlCurrSourceWorkSheet = xlSourceWorkBook.Worksheets[iIndex];
+                        if (xlCurrSourceWorkSheet.Cells[1, 1].Value != null){
+                            year = Convert.ToInt16(xlCurrSourceWorkSheet.Cells[2, 1].Value);
+                            currMessage = Convert.ToString(year) + Environment.NewLine + currFolder.Name.Substring(8);
+                            textBox2.Text = currMessage;
+                            textBox2.Refresh();
+                            for (yIndex = 1; yIndex <= yUpperLimit; yIndex++) {
+                                for (xIndex = 1; xIndex <= 12; xIndex++) {
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 1].Value = Convert.ToString(xlCurrSourceWorkSheet.Cells[2, 2 + xIndex].Value) + "/" + Convert.ToString(year);
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 2].Value = currFolder.Name.Substring(8);
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 3].Value = xlCurrSourceWorkSheet.Cells[2 + yIndex, 1].Value;
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 4].Value = xlCurrSourceWorkSheet.Cells[2 + yIndex, 2].Value;
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 5].Value = xlCurrSourceWorkSheet.Cells[2 + yIndex, 2 + xIndex].Value;
+                                    targetWBRowIndex++;
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 1].Value = xlCurrSourceWorkSheet.Cells[2, 2 + xIndex].Value + "/" + Convert.ToString(year);
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 2].Value = currFolder.Name.Substring(8);
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 3].Value = xlCurrSourceWorkSheet.Cells[yOffset + yIndex, 1].Value;
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 4].Value = xlCurrSourceWorkSheet.Cells[yOffset + yIndex, 2].Value;
+                                    xlTargetWorkSheet.Cells[targetWBRowIndex, 5].Value = xlCurrSourceWorkSheet.Cells[yOffset + yIndex, 2 + xIndex].Value;
+                                    targetWBRowIndex++;
+                                }
+                            }
+                        }
+                    }
+                }
+                xlSourceApp.Quit();
+                xlTargetWorkBook.Save();
+                xlTargetApp.Quit();
+            }
+        }
+    }
+}
